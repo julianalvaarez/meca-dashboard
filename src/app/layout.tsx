a1b4c83ed851012ed87b0491c0ee9964641af244
Julian Alvarez <julialva2008@gmail.com>
2026-02-13 00:02:24 -0300

    add: pwa

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -5,6 +5,16 @@ import { LayoutWrapper } from "@/components/LayoutWrapper";
 export const metadata: Metadata = {
   title: "La Meca CDA - Panel de Control",
   description: "Panel de Control de La Meca CDA",
+  manifest: "/manifest.json",
+  themeColor: "#000000",
+  appleWebApp: {
+    capable: true,
+    statusBarStyle: "black-translucent",
+    title: "La Meca CDA"
+  },
+  icons: {
+    apple: "/meca-logo.png"
+  }
 };
 
 export default function RootLayout({ children }: Readonly<{ children: React.ReactNode }>) {
